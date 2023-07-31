179497499aeb1c9ebe3d6617a520bc15cca3a67d
bangld <kobato2096@gmail.com>
2023-07-31 16:24:10 +0700

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -39,5 +39,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
+  matcher: ['/auth/:path*', '/settings/:path*', '/decks', '/review', '/cards'],
 };
