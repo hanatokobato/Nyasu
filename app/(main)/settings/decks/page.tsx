fd9b665f8b1729572e81f576a5dbefbbe2543677
bangld <kobato2096@gmail.com>
2023-08-21 15:17:03 +0700

    Pagination

--- app/(main)/settings/decks/page.tsx
+++ app/(main)/settings/decks/page.tsx
@@ -9,11 +9,13 @@ import { toast, ToastContainer } from 'react-toastify';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { BsSearch } from 'react-icons/bs';
 import { GiCardRandom } from 'react-icons/gi';
+import Pagination from '@/app/components/Pagination';
 
 const SettingsDecks = () => {
   const router = useRouter();
   const [decks, setDecks] = useState<IDeck[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [pageCount, setPageCount] = useState(0);
 
   const fetchDecks = useCallback(
     async (page: number = 1, perPage: number = 10): Promise<IDeck[]> => {
@@ -34,6 +36,7 @@ const SettingsDecks = () => {
         photoUrl: deck.photoUrl,
         createdAt: deck.createdAt,
       }));
+      setPageCount(response.data.total_page);
       setIsLoading(false);
 
       return fetchedDecks;
@@ -52,6 +55,14 @@ const SettingsDecks = () => {
       toast(e.message, { type: 'error' });
     }
   }, []);
+
+  const pageChangeHandler = useCallback(
+    async (selectedItem: { selected: number }) => {
+      const decks = await fetchDecks(selectedItem.selected + 1);
+      setDecks(decks);
+    },
+    [fetchDecks]
+  );
 
   const initData = useCallback(async () => {
     const initDecks = await fetchDecks();
@@ -187,62 +198,10 @@ const SettingsDecks = () => {
               </tbody>
             </table>
             <div className="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  className="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
-                >
-                  <svg
-                    width="9"
-                    fill="currentColor"
-                    height="8"
-                    className=""
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 "
-                >
-                  1
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                >
-                  2
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-gray-600 bg-white border-t border-b hover:bg-gray-100"
-                >
-                  3
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                >
-                  4
-                </button>
-                <button
-                  type="button"
-                  className="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
-                >
-                  <svg
-                    width="9"
-                    fill="currentColor"
-                    height="8"
-                    className=""
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"></path>
-                  </svg>
-                </button>
-              </div>
+              <Pagination
+                pageCount={pageCount}
+                onPageChange={pageChangeHandler}
+              />
             </div>
           </div>
         </div>
